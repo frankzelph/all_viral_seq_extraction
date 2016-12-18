1f864bfa745c8c5583c955bfac57aec0cd1a5d5c
frankzelph <fzelph@gmail.com>
2016-12-18 15:52:39 +0800

    Update blast_all_2.sh

--- blast_all_2.sh
+++ blast_all_2.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # merge read1 and read2 into one file
+# "file_list.txt" includes read1 and read2 filenames gapped with a tab character "\t" in each line.
+# The read files (NGS data) are in ".fastq.gz" file format
+# if [ ! -f ".*fasta" ] ; then
 python merge_r1_r2.py file_list.txt
+# fi
 
 # blast all fasta file in this directory
 for item in `ls `
