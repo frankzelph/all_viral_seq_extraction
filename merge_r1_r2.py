131cd56d3f7a86120262adcc9566171bb1e38341
frankzelph <fzelph@gmail.com>
2017-01-02 14:45:20 +0800

    Add files via upload

--- merge_r1_r2.py
+++ merge_r1_r2.py
@@ -1,12 +1,13 @@
 #!/bin/py
 # Convert read1 and read2 fastq.gz file into fasta file, and merged them into one.
 # Usage:
-#       python merge_r1_r2.py file_list.txt
+#       python merge_r1_r2.py read1.fastq.gz read2.fastq.gz merged.fasta
 #
 
 import sys
 import os
 import re
+import datetime
 from Bio import SeqIO
 
 
@@ -14,13 +15,26 @@ from Bio import SeqIO
 
 # function: merge two read files into one
 def merge(read1_file, read2_file, merged_file):
+    print " Merging", read1_file, "and", read2_file+"..."
     bashCommand = "cat "+read1_file+' '+read2_file +' > '+merged_file
     os.system(bashCommand)
+    print " Removing "+read1_file+" and "+read2_file+"..."
     os.system("rm -rf "+read1_file)
     os.system("rm -rf "+read2_file)
 
-# function: change the read_ids in the NGS data
-def change_rid(fasta_file, read_direct, outfile):
+# function: trim fastq, extract fasta file, and change the read_ids
+def prepare_fasta(fastq_gz_file, read_direct, outfile):
+    print " Preprocession of", fastq_gz_file
+    fasta_file = re.split('\.', fastq_gz_file)[0]+'.fasta'
+    bashcommand = "seqtk trimfq "+fastq_gz_file + ' > ' +fastq_gz_file+".fq"
+    print " "+bashcommand
+    os.system(bashcommand)
+    bashcommand = "seqtk seq -A "+fastq_gz_file+".fq" + ' > ' +fasta_file
+    print " "+bashcommand
+    os.system(bashcommand)
+    print " rm -rf "+fastq_gz_file+".fq"
+    os.system("rm -rf "+fastq_gz_file+".fq")
+    print " Assign read id with direction..."
     fh = open(fasta_file, 'r')
     fout = open(outfile, 'w')
     for record in SeqIO.parse(fh, 'fasta'):
@@ -29,32 +43,22 @@ def change_rid(fasta_file, read_direct, outfile):
         SeqIO.write(record, fout, 'fasta')
     fh.close()
     fout.close()
+    print " rm -rf "+fasta_file
     os.system("rm -rf "+fasta_file)
+    print " mv "+outfile+" "+fasta_file
     os.system("mv "+outfile+" "+fasta_file)
-        
+    return fasta_file
 
 # function: parse file_list and combine two read file into one
 def main(read1_file, read2_file, merged_file):
-	read1_fasta = re.split('\.', read1_file)[0]+'.fasta'
-        read2_fasta = re.split('\.', read2_file)[0]+'.fasta'
-        merged_file = merged_file+'.fasta'
-        # prepare read1 fasta file
-        bashcommand = "seqtk trimfq "+read1_file + ' > ' +read1_file+".fq"
-        os.system(bashcommand)
-        bashcommand = "seqtk seq -A "+read1_file+".fq" + ' > ' +read1_fasta
-        os.system(bashcommand)
-        os.system("rm -rf "+read1_file+".fq")
-        
-        change_rid(read1_fasta, "-r1", "read1_tmp.fasta")
-        # prepare read2 fasta file
-        bashcommand = "seqtk trimfq "+read2_file + ' > ' +read2_file+".fq"
-        os.system(bashcommand)
-        bashcommand = "seqtk seq -A "+read2_file+".fq" + ' > ' +read2_fasta
-        os.system(bashcommand)
-        os.system("rm -rf "+read1_file+".fq")
-        change_rid(read2_fasta, "-r2", "read2_tmp.fasta")
-        # merge the two fasta file into one
-        merge(read1_fasta, read2_fasta, merged_file)
+    print " Start from [", str(datetime.datetime.now())[:19], "]"
+    # prepare read1 fasta file
+    read1_fasta = prepare_fasta(read1_file, "-r1", "read1_tmp.fasta")
+    # prepare read2 fasta file
+    read2_fasta = prepare_fasta(read2_file, "-r2", "read2_tmp.fasta")    
+    # merge the two fasta file into one    
+    merge(read1_fasta, read2_fasta, merged_file)
+    print " Finish at [", str(datetime.datetime.now())[:19], "]"
 
 #============================ work =====================================#
 read1_file = sys.argv[1]
